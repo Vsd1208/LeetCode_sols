7a83c80e61c4d80feddafc553bc3219847f6bcab
Vsd1208 <saidhirajvandanapu@gmail.com>
2026-03-02 12:42:32 +0530

    Time: 3 ms (71.77%) | Memory: 43.9 MB (98.62%) - LeetSync

---
+++ 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int minimumPairRemoval(int[] nums) {
+
+        List<Integer> list = new ArrayList<>();
+        for (int n : nums) list.add(n);
+
+        int operations = 0;
+
+        while (!isNonDecreasing(list)) {
+
+            int minSum = Integer.MAX_VALUE;
+            int index = 0;
+
+            // Find leftmost minimum adjacent pair
+            for (int i = 0; i < list.size() - 1; i++) {
+                int sum = list.get(i) + list.get(i + 1);
+                if (sum < minSum) {
+                    minSum = sum;
+                    index = i;
+                }
+            }
+
+            // Merge that pair
+            list.set(index, minSum);
+            list.remove(index + 1);
+
+            operations++;
+        }
+
+        return operations;
+    }
+
+    private boolean isNonDecreasing(List<Integer> list) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i) > list.get(i + 1))
+                return false;
+        }
+        return true;
+    }
+}
