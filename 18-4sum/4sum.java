393610923b42b2048d22a8362940f43a1e791643
SAI DHIRAJ VANDANAPU <saidhirajvandanapu@gmail.com>
2026-03-09 20:09:41 +0530

    Time: 19 ms (80.61%) | Memory: 47.6 MB (5.90%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,57 +1,39 @@
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-
         Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList<>();
-
-        for(int i=0;i<nums.length-3;i++)
-        {
-            if(i!=0 && nums[i]==nums[i-1])
-                continue;
-
-            for(int j=i+1;j<nums.length-2;j++)
-            {
-                if(j!=i+1 && nums[j]==nums[j-1])
-                    continue;
-
-                int left=j+1;
-                int right=nums.length-1;
-
-                long t = (long)nums[i] + nums[j];
-
-                while(left<right)
-                {
-                    long sum = t + nums[left] + nums[right];
-
-                    if(sum == target)
-                    {
-                        List<Integer> l = new ArrayList<>();
-                        l.add(nums[i]);
-                        l.add(nums[j]);
-                        l.add(nums[left]);
-                        l.add(nums[right]);
-                        ans.add(l);
-
-                        left++;
-                        right--;
-
-                        while(left<right && nums[left]==nums[left-1]) left++;
-                        while(left<right && nums[right]==nums[right+1]) right--;
-                    }
-                    else if(sum < target)
-                    {
-                        left++;
-                    }
-                    else
-                    {
-                        right--;
-                    }
+        return kSum(nums, target, 0, 4);
+    }
+    private List<List<Integer>> kSum(int[] nums, long target, int start, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(k == 2){
+            int left = start, right = nums.length - 1;
+            while(left < right){
+                long sum = nums[left] + nums[right];
+                if(sum == target){
+                    res.add(Arrays.asList(nums[left], nums[right]));
+                    left++;
+                    right--;
+                    while(left < right && nums[left] == nums[left-1]) left++;
+                    while(left < right && nums[right] == nums[right+1]) right--;
                 }
+                else if(sum < target)
+                    left++;
+                else
+                    right--;
+            }
+            return res;
+        }
+        for(int i = start; i < nums.length - k + 1; i++){
+            if(i > start && nums[i] == nums[i-1])
+                continue;
+            List<List<Integer>> temp = kSum(nums, target - nums[i], i + 1, k - 1);
+            for(List<Integer> list : temp){
+                List<Integer> newList = new ArrayList<>();
+                newList.add(nums[i]);
+                newList.addAll(list);
+                res.add(newList);
             }
         }
-
-        return ans;
+        return res;
     }
 }
