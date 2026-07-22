be87e4e3dbc114401ded0704569c913459445731
SAI DHIRAJ VANDANAPU <saidhirajvandanapu@gmail.com>
2026-07-22 22:20:52 +0530

    Time: 1 ms (99.84%) | Memory: 48 MB (10.67%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.java
@@ -0,0 +1,27 @@
+class Solution {
+    private void dfs(char[][] board,int i,int j,int m,int n){
+        if(i<0||j<0||i>=m||j>=n||board[i][j]!='O') return;
+        board[i][j]='T';
+        dfs(board,i-1,j,m,n);
+        dfs(board,i+1,j,m,n);
+        dfs(board,i,j-1,m,n);
+        dfs(board,i,j+1,m,n);
+    }
+    public void solve(char[][] board) {
+        int m = board.length,n = board[0].length;
+        for(int i=0;i<m;i++){
+            if(board[i][0]=='O') dfs(board,i,0,m,n);
+            if(board[i][n-1]=='O') dfs(board,i,n-1,m,n);
+        }
+        for(int i=0;i<n;i++){
+            if(board[0][i]=='O') dfs(board,0,i,m,n);
+            if(board[m-1][i]=='O') dfs(board,m-1,i,m,n);
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]=='O') board[i][j]='X';
+                else if(board[i][j]=='T') board[i][j]='O';
+            }
+        }
+    }
+}
