c3c8c9a89ae71c9ce70825cd44abc0d81b611c9b
SAI DHIRAJ VANDANAPU <saidhirajvandanapu@gmail.com>
2026-08-04 20:32:47 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (88.97%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -1,3 +1,21 @@
+// ListNode dummy = new ListNode(-1);
+        // ListNode merged = dummy;
+        // ListNode l1 = list1;
+        // ListNode l2 = list2;
+        // while(l1 != null && l2 !=null){
+        //     if(l1.val <= l2.val){
+        //         merged.next = l1;
+        //         l1=l1.next;
+        //     }
+        //     else{
+        //         merged.next = l2;
+        //         l2=l2.next;
+        //     }
+        //     merged=merged.next;
+        // }
+        // if (l1 != null) merged.next = l1;
+        // if (l2 != null) merged.next = l2;
+        // return dummy.next;
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -10,23 +28,22 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode dummy = new ListNode(-1);
-        ListNode merged = dummy;
-        ListNode l1 = list1;
-        ListNode l2 = list2;
-        while(l1 != null && l2 !=null){
-            if(l1.val <= l2.val){
-                merged.next = l1;
-                l1=l1.next;
+        ListNode n1 = new ListNode(-1);
+        ListNode dummy = n1;
+        while(list1!=null && list2!=null){
+            if(list1.val<=list2.val){
+                n1.next=list1;
+                list1=list1.next;
             }
-            else{
-                merged.next = l2;
-                l2=l2.next;
+            else {
+                n1.next=list2;
+                list2=list2.next;
             }
-            merged=merged.next;
+            n1=n1.next;
         }
-        if (l1 != null) merged.next = l1;
-        if (l2 != null) merged.next = l2;
+        if(list1!=null) n1.next=list1;
+        if(list2!=null) n1.next=list2;
+        n1=n1.next;
         return dummy.next;
     }
 }
