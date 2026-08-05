5ecf1d57affa741f414f76b1529449b8f37bd278
SAI DHIRAJ VANDANAPU <saidhirajvandanapu@gmail.com>
2026-08-05 18:53:01 +0530

    Time: 55 ms (92.31%) | Memory: 292.4 MB (33.66%) - LeetSync

---
+++ 3561-remove-methods-from-project/remove-methods-from-project.java
@@ -0,0 +1,51 @@
+class Solution {
+
+    private void dfs(List<Integer>[] graph, boolean[] suspicious, int node) {
+        suspicious[node] = true;
+
+        for (int next : graph[node]) {
+            if (!suspicious[next]) {
+                dfs(graph, suspicious, next);
+            }
+        }
+    }
+
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+
+        // Build adjacency list
+        List<Integer>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++)
+            graph[i] = new ArrayList<>();
+
+        for (int[] edge : invocations) {
+            graph[edge[0]].add(edge[1]);
+        }
+
+        // Find all suspicious methods
+        boolean[] suspicious = new boolean[n];
+        dfs(graph, suspicious, k);
+
+        // Check if any outside method invokes a suspicious method
+        for (int[] edge : invocations) {
+            int u = edge[0];
+            int v = edge[1];
+
+            if (!suspicious[u] && suspicious[v]) {
+                List<Integer> ans = new ArrayList<>();
+                for (int i = 0; i < n; i++)
+                    ans.add(i);
+                return ans;
+            }
+        }
+
+        // Remove suspicious methods
+        List<Integer> ans = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (!suspicious[i])
+                ans.add(i);
+        }
+
+        return ans;
+    }
+}
