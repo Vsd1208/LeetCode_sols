3385868c224294d8fd1dcdb86982ca0fc2763324
SAI DHIRAJ VANDANAPU <saidhirajvandanapu@gmail.com>
2026-09-14 13:17:00 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (52.92%) - LeetSync

---
+++ 866-rectangle-overlap/rectangle-overlap.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        if(Arrays.equals(rec1,rec2)) return true;
+        if ((rec1[0] < rec2[0] && rec2[0] < rec1[2]) ||
+            (rec1[0] < rec2[2]) && (rec2[2] < rec1[2]) ||
+            (rec2[0] < rec1[0]) && (rec1[0] < rec2[2]) ||
+            (rec2[0] < rec1[2]) && (rec1[2] < rec2[2])) {
+            if ((rec1[1] < rec2[1] && rec2[1] < rec1[3]) ||
+                (rec1[1] < rec2[3]) && (rec2[3] < rec1[3]) ||
+                (rec2[1] < rec1[3]) && (rec1[1] < rec2[3]) ||
+                (rec2[1] < rec1[3]) && (rec1[3] < rec2[3])) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
